3e337e237464a9a29472da966ea82f1deadde8d1
Faiz <ansarifaizdeveloper@gmail.com>
2023-10-26 23:35:25 +0530

    Changes Done in Server Settings

--- server/app.js
+++ server/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
+const app = express();
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const app = express();
 const PORT = process.env.PORT || 9000;
 const cors = require('cors');
 
